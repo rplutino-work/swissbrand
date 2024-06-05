0216b4128af010754eebe10ae7cfa0d03819be69
Ivan Sosa <ivannsosa.24@gmail.com>
2024-06-05 10:51:46 -0300

    maquetado de ficha y minicart

---
+++ vtexio-swissbrand-main/react/components/ProductSpecification/index.js
@@ -0,0 +1,64 @@
+import {useState,useEffect} from 'react';
+import { useProduct } from "vtex.product-context";
+import styles from "./style.css";
+
+function index() {
+
+    const [tabla, setTabla] = useState([])
+
+    const productContext = useProduct();
+    const {product} = productContext
+
+    console.log('infoproduct',product)
+
+    useEffect(()=>{
+        if(product && product?.properties){
+            const fichaTecnica = product.properties.filter(spec => spec.name === 'tabla-talles')[0]?.values
+            setTabla(JSON.parse(fichaTecnica))
+        }
+    },[product])
+
+    console.log({tabla})
+
+
+  return (
+    <div className={`${styles["fichaProperties"]}`}>
+        <div className={`${styles["fichaContainer"]}`}>
+        {
+            tabla?.ficha?.map(item => (
+                <div className={`${styles["fichaContent"]}`}>
+                    <p className={`${styles["fichaLabel"]}`}>{item.label}</p>
+                    <div className={`${styles["fichaValues"]}`}>
+                        {item?.values?.map(valor => (
+                            <span className={`${styles["fichaValue"]}`}>
+                                {valor}
+                            </span>
+                        ))}
+                    </div>
+                </div>
+            ))
+        }
+        </div>
+
+        <div className={`${styles["fichaContainer"]}`}>
+                {
+                    tabla?.otros?.map(item => (
+                        <div className={`${styles["fichaContent"]}`}>
+                            <p className={`${styles["fichaLabel"]}`}>{item.label}</p>
+                            <div className={`${styles["fichaValues"]}`}>
+                                {item?.values?.map(valor => (
+                                    <span className={`${styles["fichaValue"]}`}>
+                                        {valor}
+                                    </span>
+                                ))}
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+    </div>
+    
+  )
+}
+
+export default index
